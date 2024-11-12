4bcb6d9d7b0400b822005757d0b6e905d98b292c
TravisBoltz <fariduoubdamusah@gmail.com>
2024-11-12 12:15:08 +0000

    about page v1

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="max-w-6xl mx-auto space-y-5 p-3">
+      <h1 className="text-2xl font-medium text-main">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        quis congue odio. Ut at elit quis diam sodales faucibus. Curabitur sit
+        amet nunc rutrum ipsum vehicula rutrum. Praesent sed nulla elementum
+        risus maximus commodo. In velit leo, vulputate id fermentum rhoncus,
+        iaculis vitae massa. Nunc egestas orci id lectus semper, quis imperdiet
+        tortor mattis. Ut ultricies in mi eget placerat. Duis ut nibh ac felis
+        ullamcorper iaculis. Nulla egestas a arcu et sagittis. Integer consequat
+        auctor est, id condimentum dui luctus ut. Praesent iaculis faucibus
+        elit, vitae hendrerit ligula hendrerit nec. Quisque tempor est quis
+        bibendum pharetra. Integer eu velit ut tellus malesuada viverra ut vel
+        lectus. Nulla pulvinar erat sit amet elit feugiat hendrerit.
+      </p>
+      <p>
+        Donec eget maximus velit. Aenean molestie egestas felis, vitae rutrum
+        diam ultricies ac. Aliquam ornare faucibus congue. Nam eget turpis
+        ultricies, fringilla urna ac, hendrerit felis. In blandit mauris libero,
+        nec aliquam nulla auctor a. Sed commodo sit amet ex iaculis finibus.
+        Donec mattis mi non erat tempor sodales. Sed massa felis, scelerisque
+        vitae ligula ut, dictum commodo ipsum. Sed maximus gravida tortor
+        gravida porta. Nunc consectetur interdum diam et volutpat. Suspendisse
+        dui ligula, euismod eu orci in, pellentesque vestibulum tellus.
+      </p>
+      <p>
+        Nam lacinia laoreet mauris eget volutpat. Mauris id diam rutrum,
+        tincidunt mi sed, porta augue. Curabitur pretium, metus non finibus
+        viverra, sem elit mattis nunc, eget fermentum justo ante euismod massa.
+        Sed auctor suscipit tempus. Nunc tempus arcu neque, eu bibendum mauris
+        consequat ut. Maecenas eget ante nec orci malesuada convallis eu ut
+        tellus. Aenean mi magna, mollis et elementum id, rhoncus quis nibh.
+        Quisque tempor libero nec placerat tempor. Donec volutpat aliquet odio.
+        Donec sollicitudin tortor ligula, sed sagittis odio fermentum ac.
+        Integer nulla ligula, vestibulum sed ornare ut, faucibus id mauris.
+        Mauris semper leo eget felis hendrerit interdum. Quisque vehicula
+        scelerisque mauris, et bibendum tortor facilisis sed.
+      </p>
+      <p>
+        Quisque et tempus tortor. Morbi sagittis ante dui, sit amet ultrices
+        orci viverra ac. Interdum et malesuada fames ac ante ipsum primis in
+        faucibus. Aliquam at tortor sapien. Ut non tellus porta, vehicula nibh
+        ac, egestas metus. Sed felis dolor, volutpat nec ligula quis, malesuada
+        efficitur metus. Nullam non consequat enim, eu ornare sapien. In non
+        venenatis neque.
+      </p>
+    </div>
+  );
 }
